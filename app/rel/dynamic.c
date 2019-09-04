f97c0eb16f4f6c23db0f081e3871db9d9784aa52
DarlanJurak <ti.andersondomingues@gmail.com>
2019-09-04 18:46:21 -0300

    removed unused artifacts to decrease project size

--- app/rel/dynamic.c
+++
@@ -1,9 +0,0 @@
-/* this code is compiled as position independent code */
-
-#include <hellfire.h>
-
-void func(void){
-	while(1){
-		DEBUG_ADDR = (int32_t)'a';
-	}
-}
--- platform/armversatile_demo/run-qemu.sh
+++
@@ -1,2 +0,0 @@
-qemu-system-arm -cpu arm1176 -m 256 -M versatilepb -serial stdio -kernel image.elf
-
